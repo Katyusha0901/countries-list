54812eeec15b318cf7a41a3b43e7260522cc7c3f
Ekaterina Bondareva <1978231@bk.ru>
2024-05-23 14:21:41 +0500

    cleaning project and adding api

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { RoutesObject } from "./RoutesObject";
 import { MainPage } from "./components/MainPage";
+import { useEffect, useState } from "react";
 
 export function App() {
+  const [allComments, setAllComments] = useState<Comment[]>([]);
+  const [visibleComments, setVisibleComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setAllComments(data);
+        setVisibleComments(data.slice(0, 100));
+      })
+      .catch((err) => console.log("Ошибка", err));
+  }, []);
+
   return (
     <>
       <Routes>
