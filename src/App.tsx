c39e81a4d67f02ee2616528f70e1cfdade9317d6
Ekaterina Bondareva <1978231@bk.ru>
2024-05-23 14:38:42 +0500

    cleaning project and added types and props in MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import { RoutesObject } from "./RoutesObject";
 import { MainPage } from "./components/MainPage";
 import { useEffect, useState } from "react";
+import { Country } from "./types";
 
 export function App() {
-  const [allComments, setAllComments] = useState<Comment[]>([]);
-  const [visibleComments, setVisibleComments] = useState<Comment[]>([]);
+  const [allCounties, setAllCountries] = useState<Country[]>([]);
+  const [visibleCounties, setVisibleCountries] = useState<Country[]>([]);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
       .then((data) => {
-        setAllComments(data);
-        setVisibleComments(data.slice(0, 100));
+        setAllCountries(data);
+        setVisibleCountries(data.slice(0, 100));
       })
       .catch((err) => console.log("Ошибка", err));
   }, []);
@@ -21,7 +22,16 @@ export function App() {
   return (
     <>
       <Routes>
-        <Route path={RoutesObject.mainPage} element={<MainPage />}></Route>
+        <Route
+          path={RoutesObject.mainPage}
+          element={
+            <MainPage
+              countries={allCounties}
+              visibleCountries={visibleCounties}
+              setVisibleCountries={setVisibleCountries}
+            />
+          }
+        ></Route>
       </Routes>
     </>
   );
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Country } from "../types";
 
-interface Props {}
 
-export const MainPage: React.FC<Props> = ({}) => {
+interface Props {
+  countries: Country[];
+  visibleCountries: Country[];
+  setVisibleCountries: (value: React.SetStateAction<Country[]>) => void;
+}
+
+export const MainPage: React.FC<Props> = ({
+  countries,
+  visibleCountries,
+  setVisibleCountries,
+}) => {
   return <div></div>;
 };
